6c1d306b78f0952dc387d77285498a4c07919175
Karthik Soman <karthi.soman@gmail.com>
2024-08-10 20:15:29 -0700

    adding requirements file and amended app and utility scripts

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask, Response, jsonify, request
 from webargs.flaskparser import use_kwargs
 from webargs.fields import Str
 from webargs import fields, validate
-from openai import OpenAI
+from openai import OpenAI, OpenAIError
 import json
 from utility import *
 
 app = Flask(__name__)
 
+system_prompt = HIGH_LEVEL_SUMMARY_SYSTEM_PROMPT_WITHOUT_JSON_RESPONSE
+
+try:
+    client = OpenAI(api_key = os.environ.get('API_KEY'))
+except OpenAIError as e:
+    error_message = str(e)
+    app.logger.error(f"OpenAI API Error: {error_message}")
+    client = None
+
+    
 @app.route('/', methods=['GET'])
 def home():
     endpoints = sorted(r.rule for r in app.url_map.iter_rules())
@@ -17,18 +27,23 @@ def home():
 
 @app.route("/summary", methods=["POST"])
 def summary():
+    if not client:
+        return jsonify({
+            "error": "Server configuration error.",
+            "message": "OpenAI API key is not configured in the server."
+        }), 500
     data = request.json
     if not isinstance(data, dict) or "query" not in data or "results" not in data:
         return jsonify(error="Invalid request format. Must contain 'query' and 'results' keys."), 400
 
-    prompt = get_token_limited_prompt(data)
+    prompt = get_token_limited_prompt(data, system_prompt)
     
     def generate():
         stream = client.chat.completions.create(        
             temperature=TEMPERATURE,
             model=LLM_MODEL,
             messages=[
-                {"role": "system", "content": HIGH_LEVEL_SUMMARY_SYSTEM_PROMPT},
+                {"role": "system", "content": system_prompt},
                 {"role": "user", "content": prompt}
             ],
             stream=True
--- utility.py
+++ utility.py
@@ -1,17 +1,15 @@
-from openai import OpenAI
 from dotenv import load_dotenv
 import tiktoken
 from functools import lru_cache
 import os
 
 load_dotenv(os.path.join(os.path.expanduser('~'), '.gpt_ncats.env'))
-client = OpenAI(api_key = os.environ.get('API_KEY'))
 
 LLM_MODEL = 'gpt-3.5-turbo'
 TEMPERATURE = 0.7
 MAX_TOKEN_LIMIT = 15000
 
-HIGH_LEVEL_SUMMARY_SYSTEM_PROMPT = '''
+HIGH_LEVEL_SUMMARY_SYSTEM_PROMPT_WITH_JSON_RESPONSE = '''
 You are an expert in summarizing biomedical knowledge in max 500 words.
 
 You will receive a dictionary in the following format:
@@ -28,7 +26,7 @@ In the results dictionary:
 <name of a biomedical concept> forms an answer to the query.
 The corresponding list of biological paths represents the mechanistic explanation of how that answer (i.e., the biomedical concept) relates to the query.
 
-Your task is to provide a summarized answer to the given query in 500 words. For that:
+Your task is to provide a summarized answer to the given query in 500 words. Do NOT list the results, instead you are supposed to give a biologically sound summarization for them in one paragraph. For that:
 
 - Combine all the biomedical paths corresponding to all answers into a coherent textual summary that explains the mechanistic rationale for the given answer to the query.
 - Summarize any shared paths or nodes by integrating them where applicable.
@@ -81,12 +79,75 @@ Response:
 }
 '''
 
+HIGH_LEVEL_SUMMARY_SYSTEM_PROMPT_WITHOUT_JSON_RESPONSE = '''
+You are an expert in summarizing biomedical knowledge in max 500 words.
+
+You will receive a dictionary in the following format:
+{
+  "query": "<a question of string datatype>",
+  "results": {
+    "<name of a biomedical concept>": [
+      <a list where each element represents a biological path (i.e., a sequence of edges) extracted from a knowledge graph>
+    ]
+  }
+}
+
+In the results dictionary:
+<name of a biomedical concept> forms an answer to the query.
+The corresponding list of biological paths represents the mechanistic explanation of how that answer (i.e., the biomedical concept) relates to the query.
+
+Your task is to provide a summarized answer to the given query in 500 words. Do NOT list the results, instead you are supposed to give a biologically sound summarization for them in one paragraph. For that:
+
+- Combine all the biomedical paths corresponding to all answers into a coherent textual summary that explains the mechanistic rationale for the given answer to the query.
+- Summarize any shared paths or nodes by integrating them where applicable.
+- Ensure that the summary is biologically accurate, by providing right explanations to the biomedical concepts such as genes or proteins.
+- If no context is provided, report that there is no context available for summarization.
+
+
+Example:
+{
+    "query": "what drugs may treat conditions related to Ehlers-Danlos syndrome, hypermobility type?"
+    "cyclosporine": ['cyclosporine-[treats or applied or studied to treat]-Arthralgia-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type', 
+                    'cyclosporine-[treats or applied or studied to treat]-xerophthalmia-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type', 
+                    'cyclosporine-[causes increased activity of]-COL3A1-[gene associated with condition]-Ehlers-Danlos syndrome, hypermobility type', 
+                    'cyclosporine-[treats or applied or studied to treat]-gingival overgrowth-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type'],
+    "cortisol": ['cortisol-[in clinical trials for]-osteoarthritis-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[preventative for condition]-Fatigue-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[in clinical trials for]-depressive disorder-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[treats or applied or studied to treat]-depressive disorder-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type', 
+                 'cortisol-[treats or applied or studied to treat]-anxiety-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[treats or applied or studied to treat]-Fatigue-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[treats or applied or studied to treat]-migraine disorder-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[treats or applied or studied to treat]-Chronic pain-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[affects]-TGFB1-[physically interacts with]-COL5A1-[gene associated with condition]-Ehlers-Danlos syndrome, hypermobility type',
+                 'cortisol-[treats or applied or studied to treat]-osteoarthritis-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type'],
+    "lidocaine": ['lidocaine-[preventative for condition]-Myalgia-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[treats or applied or studied to treat]-depressive disorder-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-anxiety-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-osteoarthritis-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[preventative for condition]-cardiac rhythm disease-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[treats or applied or studied to treat]-migraine disorder-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-scoliosis-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[preventative for condition]-Chronic pain-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-severe cutaneous adverse reaction-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-cardiac rhythm disease-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-Chronic pain-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-Myalgia-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[treats or applied or studied to treat]-Chronic pain-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[in clinical trials for]-migraine disorder-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type',
+                  'lidocaine-[treats or applied or studied to treat]-cardiac rhythm disease-[phenotype of]-Ehlers-Danlos syndrome, hypermobility type']
+}
+Response:
+Cyclosporine is being studied for its potential to treat arthralgia, xerophthalmia, and gingival overgrowth, all of which are phenotypes associated with Ehlers-Danlos syndrome, hypermobility type. It also causes an increase in the activity of the COL3A1 gene related to this condition. Cortisol, on the other hand, is being evaluated in clinical trials for osteoarthritis and is considered as a preventative or treatment for fatigue, depressive disorder, anxiety, migraine disorder, and chronic pain in individuals with Ehlers-Danlos syndrome, hypermobility type. It also interacts with the TGFB1 gene, which physically interacts with COL5A1, another gene associated with this syndrome. Lidocaine shows promise in preventing or treating myalgia, depressive disorder, anxiety, osteoarthritis, cardiac rhythm disease, migraine disorder, scoliosis, chronic pain, severe cutaneous adverse reactions, and myalgia in individuals with Ehlers-Danlos syndrome, hypermobility type.
+'''
+
+
 
 @lru_cache(maxsize=None)
 def count_tokens_cached(text, model):
     return count_tokens(text, model)
 
-def get_token_limited_prompt(query):
+def get_token_limited_prompt(query, system_prompt):
     results = list(query["results"].items())
     tot_results = len(results)
     
@@ -101,7 +162,7 @@ def get_token_limited_prompt(query):
 
     def is_within_limit(num_results):
         prompt = create_prompt(num_results)
-        full_prompt = HIGH_LEVEL_SUMMARY_SYSTEM_PROMPT + '\n' + prompt
+        full_prompt = system_prompt + '\n' + prompt
         return count_tokens_cached(full_prompt, LLM_MODEL) <= MAX_TOKEN_LIMIT
 
     # Binary search
